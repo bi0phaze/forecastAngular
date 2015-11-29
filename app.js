29dc826338474efdb06e626d3b86498037ad06da
Nathan Hall <nathan.mh@gmail.com>
2015-11-29 10:48:36 -0500

    rework things

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-var app = angular.module('ForecastApp', []);
+angular
+.module('ForecastApp', [
+        'ngRoute',
+      ])
+  .config(function($routeProvider) {
+      $routeProvider
+        .when('/', {
+            templateUrl: 'index.html',
+            controller: 'MainController'
+        })
+        .when('/404', {
+                        template: '<h1> Sorry, page not found!</h1>'
+                      })
+        .otherwise({ redirectTo: '/404'});
+  })
